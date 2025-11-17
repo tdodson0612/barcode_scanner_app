43e2feff0a3468e85c2903bd43b9663015d201ec
tdodson0612 <terryd0612@gmail.com>
2025-11-17 13:47:51 -0800

    ignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         applicationId = "com.wiseapps.liverwise"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
